809d75ccc3d47fe3ea022bb6187a4fc7985c3f2f
Olerg <schara@mail.ru>
2022-01-27 18:31:17 +0300

    graphql

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 const DB_NAME = 'node-todo'
 const USER_NAME = 'root'
-const PASSWORD = 'password'
+const PASSWORD = 'zaq12wsx'
 
 const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
     host: 'localhost',
